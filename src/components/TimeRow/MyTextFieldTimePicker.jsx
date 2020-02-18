fa401813b8841f1ec9153c0416b5c4070048d144
richard <richardluo0506@gmail.com>
2020-02-18 15:54:19 -0800

    fix proptypes warning

--- src/components/TimeRow/MyTextFieldTimePicker.jsx
+++ src/components/TimeRow/MyTextFieldTimePicker.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 export default function MyTextFieldTimePicker(props) {
   const { label, defaultValue } = props;
@@ -21,9 +22,15 @@ export default function MyTextFieldTimePicker(props) {
     />
   );
 }
+
 const useStyles = makeStyles(() => ({
   textField: {
     width: `100px`,
     margin: 16
   }
 }));
+
+MyTextFieldTimePicker.propTypes = {
+  label: PropTypes.string,
+  defaultValue: PropTypes.string
+};
