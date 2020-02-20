a077621547f6f3a7e6f04cbc4cebed538102a06c
Richard Luo <richard_luo@intuit.com>
2020-02-19 18:44:00 -0800

    switch laptops

--- src/popup/index.js
+++ src/popup/index.js
@@ -7,6 +7,8 @@ import Popup from '../components/Popup/Popup';
 
 browser.runtime.sendMessage({ data: 'hello' });
 
+var a = b
+
 ReactDOM.render(
   <Popup text="Ext boilerplate" />,
   document.getElementById('root')
