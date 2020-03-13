3f6a240e5314f2842f8ade1d6e43177e63317ad0
richard <richardluo0506@gmail.com>
2020-03-13 14:16:28 -0700

    deprecate common-styled-components.js and combine into theme.js

--- src/components/Branding/styled-components.js
+++ src/components/Branding/styled-components.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { color } from '../common-styled-components';
+import { color } from '../theme';
 
 export const BrandingRoot = styled.div`
   position: absolute;
